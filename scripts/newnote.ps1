92d6c4dba3f170d5e182793235dd48adc1507d91
seuerlxy6 <liuxueyan2001@gmail.com>
2025-07-01 18:41:58 +0800

    vault backup: 2025-07-01 18:41:58

--- scripts/newnote.ps1
+++ scripts/newnote.ps1
@@ -1,12 +1,12 @@
 param([string]$title)
 
 if (-not $title) {
-  Write-Error "需要参数：笔记标题，例如 ./newnote.ps1 fifo_underflow"
+  Write-Error "需要标题，例如 ./newnote.ps1 fifo_underflow"
   exit 1
 }
 
-$slug  = (Get-Date -Format "yyyyMMdd_HHmm") + "_$title.md"
-$path  = Join-Path "..\record" $slug
+$slug = (Get-Date -Format "yyyyMMdd_HHmm") + "_$title.md"
+$path = Join-Path "..\record" $slug
 
 @"
 # $title
@@ -23,4 +23,4 @@ $path  = Join-Path "..\record" $slug
 "@ | Out-File $path -Encoding utf8
 
 git add $path
-code $path     # 想用 VS Code 打开；用 Obsidian 打开就改成 obsidian://open?vault=...
+code $path        # 若想直接用 VS Code 打开
